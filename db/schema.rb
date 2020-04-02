d60f7debdd2e33e52eed75bab4386b8b55b23dba
chihaso <cycyccq@gmail.com>
2020-04-02 10:45:45 +0900

    rails db:migrate

---
+++ db/schema.rb
@@ -0,0 +1,27 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_02_014417) do
+
+  create_table "bills", force: :cascade do |t|
+    t.string "title"
+    t.string "proposer"
+    t.integer "session_number"
+    t.integer "bill_number"
+    t.text "proposal"
+    t.text "outline"
+    t.text "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
