adc566b6cc18b5b79fcbe613653c3ec6d3b2b51d
chihaso <cycyccq@gmail.com>
2020-04-02 13:32:22 +0900

    billモデルをブラウザ上で変更することはないので変更系のアクションとそのルーティング、リンク、テストを削除する

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,42 +1,13 @@
 # frozen_string_literal: true
 
 class BillsController < ApplicationController
-  before_action :set_bill, only: [:show, :edit, :update, :destroy]
+  before_action :set_bill, only: [:show]
 
   def index
     @bills = Bill.all
   end
 
   def show
-  end
-
-  def new
-    @bill = Bill.new
-  end
-
-  def edit
-  end
-
-  def create
-    @bill = Bill.new(bill_params)
-    if @bill.save
-      redirect_to @bill, notice: "Bill was successfully created."
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @bill.update(bill_params)
-      redirect_to @bill, notice: "Bill was successfully updated."
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @bill.destroy
-    redirect_to bills_url, notice: "Bill was successfully destroyed."
   end
 
   private
--- test/system/bills_test.rb
+++ test/system/bills_test.rb
@@ -11,47 +11,4 @@ class BillsTest < ApplicationSystemTestCase
     visit bills_url
     assert_selector "h1", text: "法案一覧"
   end
-
-  test "creating a Bill" do
-    visit bills_url
-    click_on "New Bill"
-
-    fill_in "Bill number", with: @bill.bill_number
-    fill_in "Outline", with: @bill.outline
-    fill_in "Proposal", with: @bill.proposal
-    fill_in "Proposer", with: @bill.proposer
-    fill_in "Session number", with: @bill.session_number
-    fill_in "Status", with: @bill.status
-    fill_in "Title", with: @bill.title
-    click_on "Create Bill"
-
-    assert_text "Bill was successfully created"
-    click_on "Back"
-  end
-
-  test "updating a Bill" do
-    visit bills_url
-    click_on "Edit", match: :first
-
-    fill_in "Bill number", with: @bill.bill_number
-    fill_in "Outline", with: @bill.outline
-    fill_in "Proposal", with: @bill.proposal
-    fill_in "Proposer", with: @bill.proposer
-    fill_in "Session number", with: @bill.session_number
-    fill_in "Status", with: @bill.status
-    fill_in "Title", with: @bill.title
-    click_on "Update Bill"
-
-    assert_text "Bill was successfully updated"
-    click_on "Back"
-  end
-
-  test "destroying a Bill" do
-    visit bills_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Bill was successfully destroyed"
-  end
 end
