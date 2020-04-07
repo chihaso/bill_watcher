bd7794850b5eb07679417502fae7b0d34562e53e
chihaso <cycyccq@gmail.com>
2020-04-07 15:46:54 +0900

    [レビューを受けての修正]ワンラインで簡潔に記述する

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,9 +2,6 @@
 
 class Bill < ApplicationRecord
   def self.save_latest_discussed_bills
-    BillScrapper.latest_discussed_bills.each do |latest_discussed_bill|
-      bill = Bill.new(latest_discussed_bill)
-      bill.save
-    end
+    BillScrapper.latest_discussed_bills.map { Bill.new(_1) }.each(&:save)
   end
 end
