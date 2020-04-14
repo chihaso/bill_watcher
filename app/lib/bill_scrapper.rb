ca5d8232a40dbd3446822a849c5f74acb32536d2
chihaso <cycyccq@gmail.com>
2020-04-14 17:34:48 +0900

    議案ページはUTF-8に変換して読み込むことになったので、SJISとして読み込むための処理を削除

--- app/lib/bill_scrapper.rb
+++ app/lib/bill_scrapper.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true, coding: shift_jis
+# frozen_string_literal: true
 
 require "open-uri"
 
@@ -10,22 +10,18 @@ class BillScrapper
 
     private
       def latest_bill_page
-        @latest_bills_page ||= open_as_SJIS(BillUri::LATEST_BILLS_URI).read
+        @latest_bills_page ||= URI.open(BillUri::LATEST_BILLS_URI).read
       end
 
       def old_bill_pages
         @old_bills_pages ||=
           BillUri.old_session_urls(extract_session_numbers).map do
-            open_as_SJIS(_1).read
+            URI.open(_1).read
           end
       end
 
-      def open_as_sjis(uri)
-        URI.open(uri).set_encoding("Shift_JIS", "Shift_JIS")
-      end
-
       def extract_session_numbers
-        latest_bills_page.scan(%r{<OPTION>.*?��}m).map{ _1.match(%r{\d{3}}m).to_s }
+        latest_bill_page.scan(%r{<OPTION>.*?回}m).map{ _1.match(%r{\d{3}}m).to_s }
       end
   end
 end
