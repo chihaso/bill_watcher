92a8711f5cfc065ed5e144e7d1df46644f832dbc
chihaso <cycyccq@gmail.com>
2020-04-15 09:39:53 +0900

    変数名が名詞の形容詞的用法の場合は前の名詞を単数形にする https://github.com/chihaso/bill_watcher/pull/33#discussion_r408052710

--- app/lib/bill_scrapper.rb
+++ app/lib/bill_scrapper.rb
@@ -5,16 +5,16 @@ require "open-uri"
 class BillScrapper
   class << self
     def latest_discussed_bills
-      BillParser.bills(latest_bills_page)
+      BillParser.bills(latest_bill_page)
     end
 
     private
       def latest_bill_page
-        @latest_bills_page ||= URI.open(BillUri::LATEST_BILLS_URI).read
+        @latest_bill_page ||= URI.open(BillUri::LATEST_BILLS_URI).read
       end
 
       def old_bill_pages
-        @old_bills_pages ||=
+        @old_bill_pages ||=
           BillUri.old_session_urls(extract_session_numbers).map do
             URI.open(_1).read
           end
