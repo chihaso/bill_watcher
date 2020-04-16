95188ab81ead68c6485d7f758e187b613f30ea9c
chihaso <cycyccq@gmail.com>
2020-04-16 19:22:39 +0900

    過去の会期番号取得の際に最新会期を除くことを明確にする https://github.com/chihaso/bill_watcher/pull/37#discussion_r409431119

--- app/lib/bill_scrapper.rb
+++ app/lib/bill_scrapper.rb
@@ -19,19 +19,23 @@ class BillScrapper
 
       def old_bill_pages
         @old_bill_pages ||=
-          BillUri.old_session_urls(extract_session_numbers).map { read_as_cp932(_1) }
+          BillUri.old_session_urls(extract_old_session_numbers).map { read_as_cp932(_1) }
       end
 
       def read_as_cp932(uri)
         URI.open(uri, "r:CP932").read
       end
 
-      def extract_session_numbers
-        session_selectbox.scan(/第(\d{3})回/).flatten.drop(1).reverse
+      def extract_old_session_numbers
+        session_numbers_excluding_latest(session_selectbox).reverse
       end
 
       def session_selectbox
         latest_bill_page.slice(%r{<SELECT NAME="kaiji".*?</SELECT>}m)
+      end
+
+      def session_numbers_excluding_latest(selectbox)
+        selectbox.scan(/第(\d{3})回/).flatten.drop(1)
       end
   end
 end
