0da813baf2f646391727e0cfd8ced11010a341bf
chihaso <cycyccq@gmail.com>
2020-04-17 19:02:24 +0900

    クラスインスタンス変数の初期化タイミングをわかりやすくする https://github.com/chihaso/bill_watcher/pull/38#commitcomment-38556810

--- app/lib/bill_scrapper.rb
+++ app/lib/bill_scrapper.rb
@@ -11,7 +11,7 @@ class BillScrapper
     private
       def all_bill_pages
         @bill_pages ||=
-          BillUri.session_urls(extract_old_session_numbers).map { read_as_cp932(_1) } << latest_bill_page
+          [latest_bill_page, *BillUri.session_urls(session_numbers_excluding_latest).map { read_as_cp932(_1) }].reverse
       end
 
       def latest_bill_page
@@ -23,16 +23,12 @@ class BillScrapper
         URI.open(uri, "r:CP932").read
       end
 
-      def extract_old_session_numbers
-        session_numbers_excluding_latest(session_selectbox).reverse
+      def session_numbers_excluding_latest
+        session_selectbox.scan(/第(\d{3})回/).flatten.drop(1)
       end
 
       def session_selectbox
         latest_bill_page.slice(%r{<SELECT NAME="kaiji".*?</SELECT>}m)
-      end
-
-      def session_numbers_excluding_latest(selectbox)
-        selectbox.scan(/第(\d{3})回/).flatten.drop(1)
       end
   end
 end
