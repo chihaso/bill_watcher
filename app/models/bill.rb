63e39f87563835f813591e9fb66863d9381ddc4d
chihaso <cycyccq@gmail.com>
2020-04-17 17:41:15 +0900

    クエリメソッドにより分岐の記述を簡潔にする https://github.com/chihaso/bill_watcher/pull/38#discussion_r410013702

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -4,21 +4,17 @@ class Bill < ApplicationRecord
   class << self
     def update_bills
       BillScrapper.all_bills.each do |bill|
-        if existing_bill = existing(bill)
-          existing_bill.update(bill)
-        else
-          Bill.new(bill).save
-        end
+        Bill.find_or_initialize_by(existing_check_hash(bill)).update(bill)
       end
     end
 
     private
-      def existing(bill)
-        Bill.find_by(
+      def existing_check_hash(bill)
+        {
           submitted_session_number: bill[:submitted_session_number],
           bill_number:              bill[:bill_number],
           discussed_session_number: bill[:discussed_session_number]
-        )
+        }
       end
   end
 end
