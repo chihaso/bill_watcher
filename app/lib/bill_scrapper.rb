a7c0f117bb1c3176f87079ec53f29c87b68f7739
chihaso <cycyccq@gmail.com>
2020-04-17 17:00:13 +0900

    最新会期のページへの重複するリクエストをなくす https://github.com/chihaso/bill_watcher/pull/38#discussion_r410005778

--- app/lib/bill_scrapper.rb
+++ app/lib/bill_scrapper.rb
@@ -5,17 +5,17 @@ require "open-uri"
 class BillScrapper
   class << self
     def all_bills
-      bill_pages.flat_map { BillParser.bills(_1) }
+      all_bill_pages.flat_map { BillParser.bills(_1) }
     end
 
     private
-      def latest_bill_page
-        @latest_bill_page ||= read_as_cp932(BillUri::LATEST_BILLS_URI)
+      def all_bill_pages
+        @bill_pages ||=
+          BillUri.session_urls(extract_old_session_numbers).map { read_as_cp932(_1) } << latest_bill_page
       end
 
-      def bill_pages
-        @bill_pages ||=
-          BillUri.session_urls(extract_all_session_numbers).map { read_as_cp932(_1) }
+      def latest_bill_page
+        @latest_bill_page ||= read_as_cp932(BillUri::LATEST_BILLS_URI)
       end
 
       def read_as_cp932(uri)
@@ -23,12 +23,16 @@ class BillScrapper
         URI.open(uri, "r:CP932").read
       end
 
-      def extract_all_session_numbers
-        session_selectbox.scan(/第(\d{3})回/).flatten.reverse
+      def extract_old_session_numbers
+        session_numbers_excluding_latest(session_selectbox).reverse
       end
 
       def session_selectbox
         latest_bill_page.slice(%r{<SELECT NAME="kaiji".*?</SELECT>}m)
+      end
+
+      def session_numbers_excluding_latest(selectbox)
+        selectbox.scan(/第(\d{3})回/).flatten.drop(1)
       end
   end
 end
