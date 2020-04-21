e1c1fe3a2891b78812ea7b251dae65a78ab3f1d7
chihaso <cycyccq@gmail.com>
2020-04-21 13:12:41 +0900

    システムに依存せずにJSTとして時刻を設定できるようにする https://github.com/chihaso/bill_watcher/pull/39#discussion_r411771505

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
+require "active_support/core_ext/time"
+
 set :output, "/Users/chibayuta/dev/myapps/bill_watcher/tmp/bill_update.log"
 set :job_template, nil
 set :environment, "development"
 ENV.each { |key, value| env(key, value) }
 
-every 1.day, at: "2:00 am" do
-  runner "Bill.update_bills"
+def jst(time)
+  Time.zone = "Asia/Tokyo"
+  Time.zone.parse(time).localtime($system_utc_offset)
+end
+
+every 1.day, at: jst("2:00 am") do
+  runner "Bill.update_all"
 end
