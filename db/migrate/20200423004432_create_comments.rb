dc737b80e290376db6df931722bc96e9c02b7142
chihaso <cycyccq@gmail.com>
2020-04-23 09:45:12 +0900

    rails g scaffold comment user_id:integer bill_id:integer description:text

---
+++ db/migrate/20200423004432_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.integer :user_id
+      t.integer :bill_id
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
