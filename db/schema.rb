3de489cba11735c9a03b2def4a8dc47ce7ecdad6
chihaso <cycyccq@gmail.com>
2020-04-23 09:49:00 +0900

    rails db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_052758) do
+ActiveRecord::Schema.define(version: 2020_04_23_004432) do
+
   create_table "bills", force: :cascade do |t|
     t.string "title"
     t.string "proposer"
@@ -25,4 +24,13 @@ ActiveRecord::Schema.define(version: 2020_04_06_052758) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "discussed_session_number"
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "bill_id"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
