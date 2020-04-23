b68578e195bc5a9e3fc7ba332e9890daaa6461f4
chihaso <cycyccq@gmail.com>
2020-04-23 11:50:28 +0900

    中身のないテストファイルを削除 https://github.com/chihaso/bill_watcher/pull/47#pullrequestreview-398720234

--- test/controllers/comments_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-
-class CommentsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @comment = comments(:one)
-  end
-end
--- test/system/comments_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "application_system_test_case"
-
-class CommentsTest < ApplicationSystemTestCase
-  setup do
-    @comment = comments(:one)
-  end
-end
