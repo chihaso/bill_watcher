5274d2a373c00b7f74a76dc0fa777d665ddd5dc8
chihaso <cycyccq@gmail.com>
2020-04-27 18:40:09 +0900

    みやすくするため、引数を文字列ではなくlambdaで渡す

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -5,7 +5,7 @@ require "test_helper"
 class CommentsControllerTest < ActionDispatch::IntegrationTest
   test "コメントの作成に成功した場合、法案詳細ページにリダイレクトされる" do
     bill = bills(:one)
-    assert_difference "bill.comments.count" do
+    assert_difference -> { bill.comments.count } do
       post bill_comments_url(bill), params: { comment: { description: "test" } }
     end
     assert_redirected_to bill_url(bill)
@@ -13,7 +13,7 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
 
   test "コメントの作成に失敗した（空のコメントを投稿した場合）場合、法案詳細ページにリダイレクトされる" do
     bill = bills(:one)
-    assert_no_difference "bill.comments.count" do
+    assert_no_difference -> { bill.comments.count } do
       post bill_comments_url(bill), params: { comment: { description: "" } }
     end
     assert_redirected_to bill_url(bill)
@@ -22,7 +22,7 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   test "コメントの更新に成功した場合、法案詳細ページにリダイレクトされる" do
     comment = comments(:one)
     bill = bills(:one)
-    assert_changes "Comment.find(comment.id).description" do
+    assert_changes -> { Comment.find(comment.id).description } do
       put bill_comment_url(bill, comment), params: { comment: { description: "test" } }
     end
     assert_redirected_to bill_url(bill)
@@ -31,7 +31,7 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   test "コメントの更新に失敗した（空のコメントを投稿した場合）場合、法案詳細ページにリダイレクトされる" do
     comment = comments(:one)
     bill = bills(:one)
-    assert_no_changes "Comment.find(comment.id).description" do
+    assert_no_changes -> { Comment.find(comment.id).description } do
       put bill_comment_url(bill, comment), params: { comment: { description: "" } }
     end
     assert_redirected_to bill_url(bill)
@@ -39,7 +39,7 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
 
   test "コメントの削除に成功した場合、法案詳細ページにリダイレクトされる" do
     bill = bills(:one)
-    assert_difference "bill.comments.count", -1 do
+    assert_difference -> { bill.comments.count }, -1 do
       delete bill_comment_url(bill, comments(:one))
     end
     assert_redirected_to bill_url(bill)
