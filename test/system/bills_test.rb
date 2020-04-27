59c924106470c608fd12e46a50355fb54b962ac6
chihaso <cycyccq@gmail.com>
2020-04-27 17:23:53 +0900

    法案に紐づけられたコメントの数と表示されたコメントの数が一致していることを確かめる記述を追加 https://github.com/chihaso/bill_watcher/pull/52#discussion_r415517350

--- test/system/bills_test.rb
+++ test/system/bills_test.rb
@@ -8,10 +8,12 @@ class BillsTest < ApplicationSystemTestCase
     assert_selector "h1", text: I18n.t("bills.index.title")
   end
 
-  test "個別の法案ページでコメントが表示される" do
+  test "個別の法案ページでその法案に紐づくコメントが全て表示される" do
     bill = bills(:one)
     visit bill_url(bill)
     assert_text "法案oneへのコメント"
+    displayed_comments = all(:css, ".comment")
+    assert_equal bill.comments.count, displayed_comments.count
   end
 
   test "個別の法案ページでコメントを追加できる" do
