ebcaaf54ecc1c9d7f76041fc05d9840c4b3d27b7
chihaso <cycyccq@gmail.com>
2020-04-27 16:16:47 +0900

    追加、削除する対象のコメントまでい確認できるテストにする https://github.com/chihaso/bill_watcher/pull/52#discussion_r415515868

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -5,7 +5,7 @@ require "test_helper"
 class CommentsControllerTest < ActionDispatch::IntegrationTest
   test "コメントの作成に成功した場合、法案詳細ページにリダイレクトされる" do
     bill = bills(:one)
-    assert_difference "Comment.count" do
+    assert_difference "bill.comments.count" do
       post bill_comments_url(bill), params: { comment: { description: "test" } }
     end
     assert_redirected_to bill_url(bill)
@@ -13,7 +13,7 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
 
   test "コメントの作成に失敗した（空のコメントを投稿した場合）場合、法案詳細ページにリダイレクトされる" do
     bill = bills(:one)
-    assert_no_difference "Comment.count" do
+    assert_no_difference "bill.comments.count" do
       post bill_comments_url(bill), params: { comment: { description: "" } }
     end
     assert_redirected_to bill_url(bill)
@@ -36,7 +36,7 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   test "コメントの削除に成功した場合、法案詳細ページにリダイレクトされる" do
     comment = comments(:one)
     bill = bills(:one)
-    assert_difference "Comment.count", -1 do
+    assert_difference "bill.comments.count", -1 do
       delete bill_comment_url(bill, comment)
     end
     assert_redirected_to bill_url(bill)
