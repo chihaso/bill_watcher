5e4c824dc4160a187485d9ddcc50c52c62aa515a
chihaso <cycyccq@gmail.com>
2020-05-20 22:36:47 +0900

    rails generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root to: "bills#index"
   resources :bills, only: [:index, :show] do
     get "page/:page", action: :index, on: :collection
