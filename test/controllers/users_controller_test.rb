2124999e317f722a33fc9c8a5d03d4b087a392b2
chihaso <cycyccq@gmail.com>
2020-05-30 18:00:14 +0900

    テスト修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,8 +3,18 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  setup do
+    sign_in users(:one)
+  end
+
   test "should get show" do
     get user_url(users(:one).id)
     assert_response :success
+  end
+
+  test "redirect to my show when request other user's" do
+    get user_url(users(:two).id)
+    assert_response :redirect
   end
 end
