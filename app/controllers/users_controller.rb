176c4fb28fc2cecdc4e3693254f2df94f1a5f899
chihaso <cycyccq@gmail.com>
2020-05-30 17:07:41 +0900

    自分以外のマイページは閲覧できない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@
 
 class UsersController < ApplicationController
   def show
-    @user = User.find params[:id]
+    @user = current_user
+    if current_user != User.find(params[:id])
+      redirect_to @user, alert: t(".cannot_get_other_user_page")
+    end
+    @user = current_user
   end
 end
