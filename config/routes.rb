6ae69b022cd3780d7298ecdb34b86f4d9af8b57c
chihaso <cycyccq@gmail.com>
2020-05-30 15:39:13 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "users/show"
   devise_for :users
   root to: "bills#index"
   resources :bills, only: [:index, :show] do
     get "page/:page", action: :index, on: :collection
     resources :comments, only: [:create, :update, :destroy]
   end
+  resources :users, only: [:show]
 end
