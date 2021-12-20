c5147d87b7bd711384e139ca0924c2751d25b501
chihaso <cycyccq@gmail.com>
2021-12-20 23:03:13 +0900

    bin/testでシステムテストも実行する
    - rails test:system時に定数Seleniumが未定義というエラーが出ていたのを修正

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "selenium-webdriver"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
