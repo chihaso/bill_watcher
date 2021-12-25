408353d7c279f486d9cf55f6bd23598cadd1262d
chihaso <cycyccq@gmail.com>
2021-12-25 10:43:21 +0900

    存在しないcssを読み込もうとしてエラーが出ていたので削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bulma/css/bulma.css")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
