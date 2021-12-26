9ba8ded148ea0eb3235880873c4cb05aac564df7
chihaso <cycyccq@gmail.com>
2021-12-26 23:01:13 +0900

    ウォッチボタンを追加
    - ウォッチボタンはログイン時のみ表示する
    - ボタン操作はAjaxにする
      - user_idなどのパラメータはdata属性を利用してJSに渡す

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
   has_many :comments
   has_many :watches, dependent: :destroy
   has_many :watching_bills, through: :watches, foreign_key: :bill_id, source: :bill
+
+  def watching_bill?(bill_id)
+    watching_bill_ids.include?(bill_id)
+  end
+
+  private
+    def watching_bill_ids
+      @watching_bill_ids ||= watching_bills.ids
+    end
 end
