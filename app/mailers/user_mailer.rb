03dd0557d8f50809b546dff7f966ac51e8175806
chihaso <cycyccq@gmail.com>
2022-01-15 17:25:19 +0900

    審議状況の更新を知らせるメールの用意
    - mailerとviewを追加
    - 拡張子がslimなのに書式がerbのままのファイルがあったので修正
    - mailer.textファイルについてはslimだと正しく描画しづらいので拡張子をerbに戻す

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+  def bill_update_email
+    @user = params[:user]
+    @bills = params[:bills]
+
+    mail(to: @user.email, subject: 'ウォッチ中の法案の審議状況が更新されました')
+  end
+end
